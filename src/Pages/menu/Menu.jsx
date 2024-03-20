0f533b3bcc487778550fc41489ececaae9b119ce
sarthak Singh <100351826+sarthaksingh007@users.noreply.github.com>
2024-03-20 12:48:49 +0530

    chnage in menu

--- src/Pages/menu/Menu.jsx
+++ src/Pages/menu/Menu.jsx
@@ -9,7 +9,7 @@ const label = { inputProps: { "aria-label": "Checkbox demo" } };
 import { useNavigate, useParams } from "react-router-dom";
 import CustomPagination from "../Otherpages/CustomPagination.jsx";
 import { FoodMenuDetails } from "./service/FoodMenu_Get.jsx";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 import { AddCart, RemoveFromCart } from "../../Redux/OrderSystem.js";
 
@@ -107,131 +107,137 @@ const Menu = () => {
     (state) => state.menu
   );
   return (
-    <section className="mb-24">
-      <h1 className="text-center font-bold text-black text-xl my-4 mx-auto">
-        {restaurantName}
-      </h1>
-      <div className="w-fit d-flex flex-col items-start justify-center rounded-lg border border-gray-900 border-opacity-17 shadow-lg sm:px-32 py-2 mx-auto px-5">
-        <p className="text-black font-semibold">
-          <span className="text-[#de4d11] font-semibold ">
-            Restaurant Name:
-          </span>{" "}
-          {restaurantName}{" "}
-        </p>
-        <p className="text-black font-semibold">
-          <span className="text-[#de4d11] font-semibold ">Station Name:</span>{" "}
-          {stationName}{" "}
-        </p>
-        <p className="text-black font-semibold">
-          <span className="text-[#de4d11] font-semibold ">Minimum Order:</span>{" "}
-          ₹{minimumCost}
-        </p>
-      </div>
-
-      <div className="my-2">
-        <ImageSlider />
-      </div>
-
-      <div>
-        {buttonsData.map((button, index) => (
-          <button
-            key={index}
-            onClick={() => handleButtonClick(button.name)}
-            className={`buttons p-2 m-2 rounded-2xl ${
-              activeButton === button.name ? "active" : ""
-            }`}
-          >
-            {button.name}
-          </button>
-        ))}
-
-        {/* Show content based on the active button */}
-        {activeButton ? (
-          <div>
-            {getCurrentContent().map((item, index) => (
-              <div
-                key={index}
-                className="d-flex shadow-custom sm:flex-row flex-col-reverse items-start justify-evenly px-2 sm:w-10/12 sm:mx-auto custommargin  rounded-lg my-4 py-3"
-              >
-                <div className="d-flex px-3 sm:w-4/12 w-full flex-col sm:items-start items-center sm:py-1 py-2 ">
-                  <h4 className="text-black font-bold">{item.food_name}</h4>
-                  <h4 className="text-black font-bold">
-                    {item.selling_price} ₹
-                  </h4>
-                  <p className="text-justify font-semibold text-gray-400 ">
-                    {item.food_discription}
-                  </p>
-                  <div
-                    className="bg-[#DE4D11] sm:w-fit w-full  text-lg text-white d-flex sm:justify-start justify-around items-center"
-                    style={{ borderRadius: "2rem" }}
-                  >
-                    <button
-                      className="bg-[#DE4D11] text-white font-semibold text-lg p-2"
-                      style={{ borderRadius: "2rem 0rem 0rem 2rem" }}
-                      onClick={() => dispatch(RemoveFromCart(item))}
-                    >
-                      -
-                    </button>
-                    <h6 className="mb-0 p-2">Add</h6>
-                    <button
-                      className="bg-[#DE4D11] text-white font-semibold text-lg p-2"
-                      style={{ borderRadius: "0rem 2rem 2rem 0rem" }}
-                      onClick={() => dispatch(AddCart(item))}
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
-                <div className="d-flex mx-auto items-start sm:justify-between justify-end md:justify-between">
-                  <img
-                    className="w-2/4 mb-2 sm:mr-0 mr-16 p-3 bg-gray-800 rounded-lg"
-                    src={Food}
-                    alt="Food"
-                  />
-                  <Checkbox
-                    style={{ color: "red" }}
-                    {...label}
-                    icon={<FavoriteBorder />}
-                    checkedIcon={<Favorite />}
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <>
-            <p className="text-center">no food menu available now.</p>
-          </>
-        )}
-      </div>
-      <CustomPagination
-        currentPage={currentPageMap[activeButton] || 1}
-        totalPages={getTotalPages()}
-        onPageChange={handlePageChange}
-      />
-      {totalQuantity > 0 && (
-        <div className="bg-gradient-to-b from-green-500 to-green-600 px-8 py-6 mt-2 flex items-center justify-between fixed sm:w-3/4 w-full bottom-0 z-20 rounded-sm">
-          <div>
-            <h5 className="text-white font-bold">
-              {totalQuantity} Items(s) Added
-            </h5>
-            <p className="text-gray-300 font-semibold mb-0">
-              {totalPrice}₹ Total items price
+    <>
+      {restaurantDetails ? (
+        <section className="mb-24">
+          <h1 className="text-center font-bold text-black text-xl my-4 mx-auto">
+            {restaurantName}
+          </h1>
+          <div className="w-fit d-flex flex-col items-start justify-center rounded-lg border border-gray-900 border-opacity-17 shadow-lg sm:px-32 py-2 mx-auto px-5">
+            <p className="text-black font-semibold">
+              <span className="text-[#de4d11] font-semibold ">
+                Restaurant Name:
+              </span>{" "}
+              {restaurantName}{" "}
+            </p>
+            <p className="text-black font-semibold">
+              <span className="text-[#de4d11] font-semibold ">
+                Station Name:
+              </span>{" "}
+              {stationName}{" "}
+            </p>
+            <p className="text-black font-semibold">
+              <span className="text-[#de4d11] font-semibold ">
+                Minimum Order:
+              </span>{" "}
+              ₹{minimumCost}
             </p>
           </div>
-          <button
-            className=" text-gray-700 bg-white font-bold p-3 rounded-lg"
-            onClick={handlesubmit}
-          >
-            View Cart
-          </button>
-        </div>
+
+          <div className="my-2">
+            <ImageSlider />
+          </div>
+
+          <div>
+            {buttonsData.map((button, index) => (
+              <button
+                key={index}
+                onClick={() => handleButtonClick(button.name)}
+                className={`buttons p-2 m-2 rounded-2xl ${
+                  activeButton === button.name ? "active" : ""
+                }`}
+              >
+                {button.name}
+              </button>
+            ))}
+
+            {/* Show content based on the active button */}
+            {activeButton ? (
+              <div>
+                {getCurrentContent().map((item, index) => (
+                  <div
+                    key={index}
+                    className="d-flex shadow-custom sm:flex-row flex-col-reverse items-start justify-evenly px-2 sm:w-10/12 sm:mx-auto custommargin  rounded-lg my-4 py-3"
+                  >
+                    <div className="d-flex px-3 sm:w-4/12 w-full flex-col sm:items-start items-center sm:py-1 py-2 ">
+                      <h4 className="text-black font-bold">{item.food_name}</h4>
+                      <h4 className="text-black font-bold">
+                        {item.selling_price} ₹
+                      </h4>
+                      <p className="text-justify font-semibold text-gray-400 ">
+                        {item.food_discription}
+                      </p>
+                      <div
+                        className="bg-[#DE4D11] sm:w-fit w-full  text-lg text-white d-flex sm:justify-start justify-around items-center"
+                        style={{ borderRadius: "2rem" }}
+                      >
+                        <button
+                          className="bg-[#DE4D11] text-white font-semibold text-lg p-2"
+                          style={{ borderRadius: "2rem 0rem 0rem 2rem" }}
+                          onClick={() => dispatch(RemoveFromCart(item))}
+                        >
+                          -
+                        </button>
+                        <h6 className="mb-0 p-2">Add</h6>
+                        <button
+                          className="bg-[#DE4D11] text-white font-semibold text-lg p-2"
+                          style={{ borderRadius: "0rem 2rem 2rem 0rem" }}
+                          onClick={() => dispatch(AddCart(item))}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    <div className="d-flex mx-auto items-start sm:justify-between justify-end md:justify-between">
+                      <img
+                        className="w-2/4 mb-2 sm:mr-0 mr-16 p-3 bg-gray-800 rounded-lg"
+                        src={Food}
+                        alt="Food"
+                      />
+                      <Checkbox
+                        style={{ color: "red" }}
+                        {...label}
+                        icon={<FavoriteBorder />}
+                        checkedIcon={<Favorite />}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <>
+                <p className="text-center">no food menu available now.</p>
+              </>
+            )}
+          </div>
+          <CustomPagination
+            currentPage={currentPageMap[activeButton] || 1}
+            totalPages={getTotalPages()}
+            onPageChange={handlePageChange}
+          />
+          {totalQuantity > 0 && (
+            <div className="bg-gradient-to-b from-green-500 to-green-600 px-8 py-6 mt-2 flex items-center justify-between fixed sm:w-3/4 w-full bottom-0 z-20 rounded-sm">
+              <div>
+                <h5 className="text-white font-bold">
+                  {totalQuantity} Items(s) Added
+                </h5>
+                <p className="text-gray-300 font-semibold mb-0">
+                  {totalPrice}₹ Total items price
+                </p>
+              </div>
+              <button
+                className=" text-gray-700 bg-white font-bold p-3 rounded-lg"
+                onClick={handlesubmit}
+              >
+                View Cart
+              </button>
+            </div>
+          )}
+        </section>
+      ) : (
+        <p>No food menu found</p>
       )}
-    </section>
+    </>
   );
 };
-Menu.propTypes = {
-  resturant_id: PropTypes.number.isRequired,
-};
-
 export default Menu;
